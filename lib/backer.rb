7f0000ea09b833f58785c0fe0c234664f6adb8e4
Ruth Kebede <rutht.kebede@gmail.com>
2020-03-20 20:25:28 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -10,7 +10,7 @@ class Backer
   
   def back_project(project)
     @backed_projects << project
-    Project.add_backer(project) << self
+    @backed_projects << Project.add_backer(project)
   end 
   
   
