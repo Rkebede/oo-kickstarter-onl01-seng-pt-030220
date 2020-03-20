019cd0d5fa615e9010dc96e84436908271e67c04
Ruth Kebede <rutht.kebede@gmail.com>
2020-03-20 20:15:02 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,7 +2,7 @@ class Backer
   
   attr_reader :name, :backed_projects, :title
   
-  def initialize(name, title)
+  def initialize(name)
     @name = name 
     @backed_projects = [ ]
     @title = title
